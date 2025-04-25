19812381335c9ea5dbcf6b33854a94eda45d8798
Sandeepa Lakshan <sandeepalakshan456@gmail.com>
2025-04-25 22:05:27 +0530

    feedback

--- interviewprep/components/InterviewCard.tsx
+++ interviewprep/components/InterviewCard.tsx
@@ -4,8 +4,11 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+    const feedback = userId && id
+    ? await getFeedbackByInterviewId({ interviewId: id, userId})
+    : null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formatedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
   return (
